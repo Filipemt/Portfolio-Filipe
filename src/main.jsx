9ea7c1d82c8a97c39e806fd10c9671f8587e2b17
Filipe <filipera3@gmail.com>
2024-11-18 16:25:30 -0300

    setting project

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2024 Filipe Mota
+ * @license Apache-2.0
+ */
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
