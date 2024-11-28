d602e1844ae20106c3a235cbb6bf462862b847ab
Filipe <filipera3@gmail.com>
2024-11-28 14:41:33 -0300

    create: add contact component and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Hero from "./components/Hero"
 import About from "./components/About"
 import Skill from "./components/Skill"
 import Project from "./components/Project"
+import Contact from "./components/Contact"
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
     <About/>
     <Skill/>
     <Project/>
+    <Contact/>
     </main>
     </>
   )
